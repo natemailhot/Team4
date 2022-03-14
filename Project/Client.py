62b3ca39a157df724f8d3a94584484f7c897337d
maxsicherman <97261414+maxsicherman@users.noreply.github.com>
2022-03-13 23:19:01 -0700

    Added gamemodes and fixed server

--- Project/Client.py
+++ Project/Client.py
@@ -2,6 +2,7 @@
 #import paho.mqtt.client as mqtt
 import pygame
 from network import Network
+import time
 
 ## MQTT Functions
 # def on_connect(client, userdata, flags, rc):
@@ -25,7 +26,33 @@ pygame.font.init()
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Client")
+MODES = {pygame.K_k: 'Keyboard', pygame.K_i: 'IMU', pygame.K_c: 'Camera', pygame.K_s: 'Speech'}
 
+
+class Button:
+    def __init__(self, text, x, y, color):
+        self.text = text
+        self.x = x
+        self.y = y
+        self.color = color
+        self.width = 150
+        self.height = 100
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+        font = pygame.font.SysFont("comicsans", 40)
+        text = font.render(self.text, 1, (255,255,255))
+        win.blit(text, (self.x + round(self.width/2) - round(text.get_width()/2), self.y + round(self.height/2) - round(text.get_height()/2)))
+
+    def click(self, pos):
+        x1 = pos[0]
+        y1 = pos[1]
+        if self.x <= x1 <= self.x + self.width and self.y <= y1 <= self.y + self.height:
+            return True
+        else:
+            return False
+
+btns = [Button("Keyboard", 50, 350, (0,0,0)), Button("IMU", 300, 350, (255,0,0)), Button("Speech", 550, 350, (0,255,0)), Button("Camera", 800, 350, (255,255,0))]
 
 def draw_window(WIN, game):
 
@@ -41,9 +68,12 @@ def draw_window(WIN, game):
         centerRect = text.get_rect(center = (WIDTH/2, HEIGHT/2))
         WIN.blit(text, centerRect)
     pygame.display.update()
+
+
+
     
 
-def main():
+def main(MODE):
     n = Network()
     player = int(n.getP())
     print("You are player", player)
@@ -67,11 +97,11 @@ def main():
 
             font = pygame.font.SysFont('comicsansms', 64)
 
-            if game.p1():
+            if game.getP1():
                 p1Text = font.render('Player 1 is Correct!', True, (255,255,255))
             else:
                 p1Text = font.render('Player 1 is Incorrect.', True, (255,255,255))
-            if game.p2():
+            if game.getP2():
                 p2Text = font.render('Player 2 is Correct!', True, (255,255,255))
             else:
                 p2Text = font.render('Player 2 is Incorrect.', True, (255,255,255))
@@ -99,14 +129,16 @@ def main():
         
         if game.connected():
             if game.getWent(player):
+                WIN.fill((0,0,0))
                 font = pygame.font.SysFont('comicsansms', 64)
                 text = font.render("Waiting...", True, (255,255,255))
                 WIN.blit(text, text.get_rect(center = (WIDTH/2, HEIGHT/2)))
                 pygame.display.update()
             else:
                 draw_window(WIN, game)
-                game.play(player)
-                game.went[player] = True        
+                game.playSound()
+                ans = game.play(player, MODE)
+                n.send(ans)        
         else:
             draw_window(WIN, game)
 
@@ -123,24 +155,37 @@ def menu_screen():
 
 
         font = pygame.font.SysFont('comicsansms', 64)
-        subfont = pygame.font.SysFont('comicsansms', 48)
+        subfont = pygame.font.SysFont('comicsansms', 40)
         text = font.render('PitchPerfect.io', False, (255,255,255))
-        subtext = subfont.render('Click to Start!', False, (255,255,255))
+        subtext = subfont.render("Mode: (K)eyboard, (I)MU, (C)amera, (S)peech", False, (255,255,255))
         centerRect = text.get_rect(center = (WIDTH/2, HEIGHT/2))
         subRect = subtext.get_rect(center = (WIDTH/2, HEIGHT/2 + 100))
         WIN.blit(text, centerRect)
         WIN.blit(subtext, subRect)
+        # for btn in btns:
+        #     btn.draw(WIN)
         pygame.display.update()
 
 
 
         for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                run = False
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-    main()
+            # if event.type == pygame.MOUSEBUTTONDOWN:
+            #     pos = pygame.mouse.get_pos()
+            #     for btn in btns:
+            #         if btn.click(pos):
+            #             MODE = btn.text
+            #             print(MODE)
+            #             run = False
+            #             break
+            if event.type == pygame.KEYDOWN: 
+                MODE = MODES[event.key]
+                print(MODE)
+                run = False
+    pygame.time.wait(2500)
+    main(MODE)
 
 while True:
     menu_screen()
--- Project/game.py
+++ Project/game.py
@@ -3,8 +3,12 @@ import os
 import random
 from pydub import AudioSegment
 from pydub.playback import play
-#import comm
-#import UI
+
+## Import these to test
+
+#import IMU_code
+#import camera
+#import medium_mode_DandF as speech
 
 
 class Game:
@@ -16,25 +20,16 @@ class Game:
         self.answers = ['a', 'a']
         self.keySol = ''
         self.sounds = ['Notes/D3.wav', 'Notes/F3.wav']
-        self.melody = []
+        self.melody = [1, 1]
         self.melodySize = 2
         self.p1 = False
         self.p2 = False
-    
 
-    def playSound(self):
         for i in range(self.melodySize):
-            sound = AudioSegment.from_wav(self.sounds[self.melody[i]])
-            play(sound)
-
-    def makeMelody(self):
-        for i in range(self.melodySize):
-            self.melody.append(random.randint(0, len(self.sounds)-1))
-        
+            self.melody[i] = random.randint(0, len(self.sounds)-1)
         relation = [0]*(self.melodySize-1)
         for i in range(self.melodySize-1):
             relation[i] = self.melody[i+1] - self.melody[i]
-
         for i in range(len(relation)):
             if relation[i] < 0:
                 self.keySol += 'v'
@@ -42,29 +37,73 @@ class Game:
                 self.keySol += '>'
             elif relation[i] > 0:
                 self.keySol += '^'
+    
 
-    def play(self, player):
-        self.makeMelody()
-        self.playSound()
-#        print(player)
+    def playSound(self):
+        for i in range(self.melodySize):
+            sound = AudioSegment.from_wav(self.sounds[self.melody[i]])
+            play(sound)
 
-        if self.modes[player] == "Keyboard":
+    # def makeMelody(self):
+    #     for i in range(self.melodySize):
+    #         self.melody.append(random.randint(0, len(self.sounds)-1))
+        
+    #     relation = [0]*(self.melodySize-1)
+    #     for i in range(self.melodySize-1):
+    #         relation[i] = self.melody[i+1] - self.melody[i]
+
+    #     for i in range(len(relation)):
+    #         if relation[i] < 0:
+    #             self.keySol += 'v'
+    #         elif relation[i] == 0:
+    #             self.keySol += '>'
+    #         elif relation[i] > 0:
+    #             self.keySol += '^'
+
+    def getP1(self):
+        return(self.p1)
+
+    def getP2(self):
+        return(self.p2)
+    
+    def printMelody(self):
+        print(self.melody)
+        return
+
+
+    def play(self, player, mode):
+
+        print(mode)
+        if mode == 'Keyboard':
             self.answers[player] = input('^, >, v')
-            if self.answers[player] == self.keySol:
-                if player == 0:
-                    self.p1 = True
-                else:
-                    self.p2 = True
-            self.went[player] = True
 
-        if self.modes[player] == "IMU":
-            self.answers[player] = IMU_code.run(...)
-            if self.answers[player] == self.imuSol:
+        ## UNCOMMENT TO TEST
+
+        # elif mode == 'IMU':
+        #     self.answers[player] = IMU_code.main()
+        # elif mode == 'Camera':
+        #     self.answers[player] = camera.level_three()
+        # elif mode == 'Speech':
+        #     self.answers[player] = speech.TODO()
+
+        return(self.answers[player])
+            
+
+    def check(self, player, ans):
+        self.answers[player] = ans
+        if self.modes[player] == "Keyboard" or self.modes[player] ==  "IMU": 
+            if ans == self.keySol:
                 if player == 0:
                     self.p1 = True
                 else:
                     self.p2 = True
-            self.went[player] = True
+        if self.modes[player] == "Camera" or self.modes[player] == "Speech":
+            if ans == self.melody:
+                if player == 0:
+                    self.p1 = True
+                else:
+                    self.p2 = True
+        self.went[player] = True
     
     def getWent(self, player):
         return(self.went[player])
@@ -83,120 +122,3 @@ class Game:
         self.went = [False, False]
         self.p1 = False
         self.p2 = False
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## MQTT initialization
-
-#client = comm.init()
-
-## Game
-
-# ROUND = 1
-# on = True
-# melody = []
-# sounds = ['Notes/C3.wav', 'Notes/C#3.wav', 'Notes/D3.wav', 'Notes/D#3.wav',
-#         'Notes/E3.wav', 'Notes/F3.wav', 'Notes/F#3.wav', 'Notes/G3.wav',
-#         'Notes/G#3.wav', 'Notes/A3.wav', 'Notes/A#3.wav', 'Notes/B3.wav',
-#         'Notes/C4.wav', 'Notes/C#4.wav', 'Notes/D4.wav', 'Notes/D#4.wav',
-#         'Notes/E4.wav', 'Notes/F4.wav', 'Notes/F#4.wav', 'Notes/G4.wav',
-#         'Notes/G#4.wav', 'Notes/A4.wav', 'Notes/A#4.wav', 'Notes/B4.wav']
-# seed = 0
-# PHASE = 1
-
-# def main():
-#     ROUND = 1
-#     on = True
-#     melody = []
-#     sounds = ['Notes/C3.wav', 'Notes/C#3.wav', 'Notes/D3.wav', 'Notes/D#3.wav',
-#             'Notes/E3.wav', 'Notes/F3.wav', 'Notes/F#3.wav', 'Notes/G3.wav',
-#             'Notes/G#3.wav', 'Notes/A3.wav', 'Notes/A#3.wav', 'Notes/B3.wav',
-#             'Notes/C4.wav', 'Notes/C#4.wav', 'Notes/D4.wav', 'Notes/D#4.wav',
-#             'Notes/E4.wav', 'Notes/F4.wav', 'Notes/F#4.wav', 'Notes/G4.wav',
-#             'Notes/G#4.wav', 'Notes/A4.wav', 'Notes/A#4.wav', 'Notes/B4.wav']
-#     seed = 0
-#     PHASE = 1
-        
-
-
-#     pygame.init()
-#     clock = pygame.time.Clock()
-#     while on:
-#         clock.tick(FPS)
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 on = False
-#                 break
-
-#         UI.draw_window(PHASE)
-
-#         if PHASE == 'START':
-#             for event in pygame.event.get():
-#                 if event.type == pygame.MOUSEBUTTONDOWN:
-#                     PHASE = 'PLAY'
-#                     continue
-
-#         if PHASE == 'PLAY':
-#             random.seed(seed)
-#             melodySize = ROUND+1
-#             for i in range(melodySize):
-#                 melody.append(random.randint(0, len(sounds)-1))
-
-#             relation = [0]*(melodySize-1)
-#             for i in range(melodySize-1):
-#                 relation[i] = melody[i+1] - melody[i]
-
-#             sol = ''
-#             for i in range(len(relation)):
-#                 if relation[i] < 0:
-#                     sol += 'v'
-#                 elif relation[i] == 0:
-#                     sol += '>'
-#                 elif relation[i] > 0:
-#                     sol += '^'
-
-#         ## Publish the melody and correct answer
-#         #    client.publish('ece180d/test',str(melody), qos=1)
-#         #    client.publish('ece180d/test', sol, qos=1)
-
-#         ## Here's how the keyboard input is processed
-
-#             for i in range(melodySize):
-#                 sound = AudioSegment.from_wav(sounds[melody[i]])
-#                 play(sound)
-
-#         ## Answer's made on client side instead of this:
-
-#             ans = input('Pitch movement (^/v/>): ')
-            
-#         ## How to store subscribed message?
-#         #    ans = client.loop_start()
-
-#             if ans == sol:
-#                 print('Correct!')
-#                 ROUND += 1
-#             else:
-#                 print('Incorrect, the correct answer is : ' + sol)
-#                 again = input('Play Again? (Y/N)')
-#                 if again == 'N':
-#                     on = False
-#                 ROUND = 1
-#             seed += 1
-#     pygame.quit()
-
-# if __name__ == "__main__":
-#     main()
