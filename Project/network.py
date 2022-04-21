54a33519613f2473344e605e998335992474fe3a
maxsicherman <97261414+maxsicherman@users.noreply.github.com>
2022-04-21 14:25:51 -0700

    Made phases and board

--- Project/network.py
+++ Project/network.py
@@ -4,7 +4,7 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "172.30.10.235"
+        self.server = "172.30.42.49"
         self.port = 5555
         self.addr = (self.server, self.port)
         self.p = self.connect()
@@ -15,7 +15,6 @@ class Network:
     def connect(self):
         try:
             self.client.connect(self.addr)
-            print("WOOO")
             return self.client.recv(2048).decode()
         except:
             pass
